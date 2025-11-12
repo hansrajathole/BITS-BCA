3e4051788ffb5ec58d02a22bdcb82828b65924bf
hansrajathole <hansrajathole1@gmail.com>
2025-11-12 20:02:28 +0530

    Add initial HTML and JavaScript files with basic structure and logging

---
+++ lec 2 js/script.js
@@ -0,0 +1,13 @@
+console.log("javaScript connected");
+console.log("hello guysssss");
+
+
+var a = 10
+var str = 'hello'
+var bool = true
+
+console.log(typeof a);
+console.log(typeof str);
+console.log(typeof bool);
+
+
