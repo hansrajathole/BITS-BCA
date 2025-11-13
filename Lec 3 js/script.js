13be89fcf0f8f528eaed0595ac7104aad9485e64
hansrajathole <hansrajathole1@gmail.com>
2025-11-13 20:00:47 +0530

    Add initial HTML and JavaScript files with function examples and comments

---
+++ Lec 3 js/script.js
@@ -0,0 +1,131 @@
+
+
+// Function declaration
+// sum(10,20)
+// function sum(a, b) {
+  
+
+//     console.log(a+b);
+    
+// }
+
+
+
+
+
+// Function expression 
+// fun()
+// let fun = function (){
+//     console.log("kaise ho");
+    
+// }
+
+// fun()
+
+
+
+// Arrow Function
+// let chacha = (msg)=>{
+//     console.log(msg);
+    
+// }
+
+// console.log(chacha);
+
+
+// chacha("chacha o chacha ****")
+
+
+
+
+// Default Parameters
+// let chacha = (age , name = "dallu")=>{
+//     console.log("hello ", name);
+//     console.log("chacha's age ", age);
+    
+// }
+
+// chacha(69 )
+
+
+
+
+
+// return keyword
+
+// function sum(x, y) {
+//     return x+y
+// }
+
+// let ans = sum(56, 56)
+
+// console.log(ans);
+
+
+
+
+// Array
+
+// let arr = [10 ,15 , 20, 25 ,30, 35 , 40, 45, 50]
+
+// console.log(arr[1]);
+
+// for(let i = 0 ; i<arr.length ; i++){
+//     console.log(i , " ===> ", arr[i]);
+    
+// }
+
+
+// Arrays Methods
+// arr.push(60)
+// let x = arr.pop()
+// console.log(x);
+// arr.shift()
+// arr.unshift(5)
+
+
+// console.log(arr.slice(0,2));
+// arr.splice(0, 3)
+// console.log(arr.indexOf(30));
+// console.log(arr.includes(31));
+
+
+// console.log(arr);
+
+
+
+// let newArr = arr.map((elem , index)=>{
+//     return elem + 5
+// })
+
+// console.log(newArr);
+
+
+// let filteredArr = arr.filter((elem , index)=>{
+//     if(elem % 2 !== 0){
+//         return elem
+//     }
+// })
+
+
+// console.log(filteredArr);
+
+
+// let ans = arr.reduce((prev , current , index )=>{
+
+//     console.log("previous ===>", prev, "current ===>" ,current);
+    
+//    return prev + current
+// })
+
+
+// console.log(ans);
+
+
+// let newArr = arr.forEach((elem , index)=>{
+//     console.log(elem);
+ 
+// })
+// console.log(newArr);
+
+
