ad4f9a3af0d50cc8eacabfc255b27f4d524e350d
hansrajathole <hansrajathole1@gmail.com>
2025-11-14 19:50:07 +0530

    Add initial HTML and JavaScript files with basic structure and comments

---
+++ Lec 4 js/script.js
@@ -0,0 +1,107 @@
+
+
+// console.log(this);
+
+
+// let person = {
+//     fName : "Deepak",
+//     mobile : 8876983245,
+//     "Roll No." : 943265,
+//     address : {
+//         state : "Bihar",
+//         city : "patna",
+//         pin : 387456
+//     },
+//     fun :function (){
+//         console.log(this.fName);
+        
+//     }
+// }
+
+// console.log(person.fName);
+
+
+
+// person.fun()
+// console.log(typeof person);
+
+
+// console.log(person.name);
+// console.log(person["Roll No."]);
+// console.log(person.address.city);
+
+
+// console.log(person.mobile);
+// person.mobile = 9237589325743
+// console.log(person.mobile);
+
+
+// delete person.address.pin
+
+
+// person.lName = "kumar"
+
+// console.log(person);
+
+
+let str = "Helllo-Guys"
+
+// str = str.toUpperCase()
+
+// console.log(str);
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str.slice(-4,6));
+// console.log(str.substring(0,6));
+
+// console.log(str.indexOf("G"));
+// console.log(str.includes("e"));
+
+// console.log(str);
+// console.log(str.trim());
+
+
+// console.log(str.replace("l","e"));
+// console.log(str.split("-"));
+
+// console.log(str.charAt(1));
+// console.log(str[0]);
+
+
+
+let date = new Date()
+
+console.log(date.toTimeString());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getMinutes());
+console.log(date.getTime());
+console.log(date.getDate());
+
+date.setFullYear(2020)
+date.setMonth(1)
+console.log(date.getFullYear());
+console.log(date.getMonth());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
