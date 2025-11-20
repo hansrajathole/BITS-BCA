339cb0b298d854c529e95ccad65a8cd1edd9ef41
hansrajathole <hansrajathole1@gmail.com>
2025-11-20 19:26:10 +0530

    Add initial HTML, CSS, and JavaScript files with basic structure and comments

---
+++ Lec 8 dom/script.js
@@ -0,0 +1,66 @@
+
+// let span = document.getElementById("span1")
+
+// let box = document.getElementsByClassName("box")
+// console.log(span);
+// console.log(box);
+// let div = document.getElementsByTagName("div")
+// console.log(div);
+
+// let div = document.querySelector('#span1')
+
+// let div = document.querySelectorAll(".box")
+// console.log(div);
+
+let para = document.querySelector("p")
+
+// h1.innerText = "hello dosto..."
+// h1.textContent = "hii all"
+// h1.innerHTML = "kaise ho"
+
+
+// console.log(para.innerText);
+// console.log(para.textContent);
+// console.log(para.innerHTML);
+
+// para.innerText = "<b >whats up</b>"
+// para.innerHTML = "<b >whats up</b>"
+
+// let name = "Raj"
+
+
+// para.innerHTML = `hello <b >${name}</b>`
+
+
+// para.style.backgroundColor = "red"
+// para.style.color = "black"
+
+// div.style.height =200+"px"
+// div.style.width = 200+"px"
+
+// div.style.backgroundColor = "green"
+
+
+
+// let div = document.querySelector("div")
+
+
+// div.classList.add("box")
+// div.classList.remove("box")
+
+// console.log(div);
+
+
+
+// let btn = document.querySelector("button")
+let body = document.querySelector("body")
+
+
+
+// btn.addEventListener("click",()=>{
+//     body.classList.toggle("dark")
+// })
+
+
+
+console.log(body.classList.contains("dark"));
