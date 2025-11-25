0db92b6766ab26cc125fa41946eefb54f53a4743
hansrajathole <hansrajathole1@gmail.com>
2025-11-25 20:00:14 +0530

    Add math module and file system operations examples in app.js

---
+++ Lec 11 node/app.js
@@ -0,0 +1,94 @@
+// console.log("hello world");
+
+// let sum = require("./math")
+
+
+// let ans = sum(56 , 98)
+
+// console.log(sum(56 , 98));
+// console.log(sum(56 , 100));
+// console.log(sum(69 , 55));
+// console.log(sum(20 , 30));
+
+
+let fs = require("fs")
+
+
+// fs.writeFile("text.txt","hello world" ,(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("file was created successfully...");
+        
+//     }
+// })
+
+
+
+// fs.appendFile("text.txt"," from file system",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("data appended successful...");
+        
+//     }
+// })
+
+
+
+// fs.readFile("text.txt","utf-8",(err , data)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log(data);
+        
+//     }
+// })
+
+
+// fs.unlink("text.txt",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("file remove successfully....");
+        
+//     }
+// })
+
+
+// fs.mkdir("chacha",{recursive : true}, (err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("folder created successfully....");
+        
+//     }
+// })
+
+// fs.readdir("chacha",{withFileTypes : true} ,(err , files)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log(files);
+        
+//     }
+// })
+
+
+
+// fs.rmdir("chacha", {recursive : true},(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("remove folder successfully...");
+        
+//     }
+// })
+
