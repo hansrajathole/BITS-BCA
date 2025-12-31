011e62668ebd86d28a9896927f3bcdcfd9472a38
hansrajathole <hansrajathole1@gmail.com>
2025-12-31 18:41:06 +0530

    feat: refactor timer functionality and enhance button styles

--- Timer/src/App.jsx
+++ Timer/src/App.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 
 const App = () => {
-
-  const date = new Date()
-
-  console.log();
-  
-
-  const [seconds, setSeconds] = useState(0)
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setisRunning] = useState(false);
 
   useEffect(() => {
-    let timer = setInterval(() => {
-      setSeconds((prev)=>prev+1)
-    }, 1000);
 
-
-    return () => {
-      clearInterval(timer)
+    let timer 
+    if (isRunning) {
+      timer = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
     }
 
-  }, [])
-  
+    return () => {
+      clearInterval(timer);
+    };
 
-  let hours = Math.floor(seconds / 3600)
-  let minutes = Math.floor((seconds%3600)/60 )
-  let secs = seconds % 60
 
+  }, [isRunning]);
+
+  let hours = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds % 3600) / 60);
+  let secs = seconds % 60;
 
   return (
     <div>
       <h1>
-        {hours} : {minutes} : {secs}
+        {String(hours).padStart(2, 0)} : {String(minutes).padStart(2, 0)} :{" "}
+        {String(secs).padStart(2, 0)}
       </h1>
+      <div>
+        <button className="start"
+        onClick={()=>{
+          setisRunning((prev)=> !prev)
+        }}
+        >{isRunning ? "Stop" : "Start"}</button>
+       
+        <button className="reset"
+        onClick={()=>{
+          setSeconds(0)
+          setisRunning(false)
+        }}
+        >Reset</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
